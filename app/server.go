624dec8282e96ca0cd6e9e75b5bc7afbc342a952
Zekihan AZMAN <zekihan_azman@hotmail.com>
2024-03-14 00:01:41 +0300

    7th stage

--- app/server.go
+++ app/server.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"net/http"
 	"os"
+	"path"
 	"strings"
 )
 
 const crlf = "\r\n"
-const lf = "\n"
+
+var dir = "./"
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
+
+	dirFlag := flag.String("directory", "", "The directory to serve files from. Defaults to the current directory.")
+	flag.Parse()
+	if dirFlag != nil && *dirFlag != "" {
+		dir = *dirFlag
+	}
+
+	fmt.Println("Serving files from", dir)
 
 	l, err := net.Listen("tcp", "127.0.0.1:4221")
 	if err != nil {
@@ -30,7 +41,6 @@ func main() {
 		}
 		go handleRequest(conn)
 	}
-
 }
 
 func handleRequest(conn net.Conn) {
@@ -41,17 +51,30 @@ func handleRequest(conn net.Conn) {
 		return
 	}
 	parseHttpResponse := parseHttp(string(read))
-	path := parseHttpResponse.Path
+	urlPath := parseHttpResponse.Path
 
 	content := response(http.StatusNotFound)
-	if path == "/" {
+	if urlPath == "/" {
 		content = response(http.StatusOK)
-	} else if strings.HasPrefix(path, "/echo/") {
-		params := strings.TrimPrefix(path, "/echo/")
+	} else if strings.HasPrefix(urlPath, "/echo/") {
+		params := strings.TrimPrefix(urlPath, "/echo/")
 		content = responseWithBody(http.StatusOK, params)
-	} else if strings.HasPrefix(path, "/user-agent") {
+	} else if strings.HasPrefix(urlPath, "/user-agent") {
 		userAgent := parseHttpResponse.Headers["User-Agent"]
 		content = responseWithBody(http.StatusOK, userAgent)
+	} else if strings.HasPrefix(urlPath, "/files/") {
+		filePath := strings.TrimPrefix(urlPath, "/files/")
+		localFilePath := path.Join(dir, filePath)
+		// Check if file exists
+		if stat, err := os.Stat(localFilePath); os.IsNotExist(err) || stat.IsDir() {
+			content = response(http.StatusNotFound)
+		} else {
+			file, err := os.ReadFile(localFilePath)
+			if err != nil {
+				content = response(http.StatusInternalServerError)
+			}
+			content = responseWithFile(http.StatusOK, string(file))
+		}
 	}
 
 	_, err = conn.Write([]byte(content))
@@ -71,7 +94,7 @@ func parseHttp(s string) ParseHttpResponse {
 	split := strings.Split(s, crlf)
 	startLine := strings.Split(split[0], " ")
 	method := startLine[0]
-	path := startLine[1]
+	urlPath := startLine[1]
 	headers := make(map[string]string)
 	for i := 1; i < len(split); i++ {
 		if split[i] == "" {
@@ -82,7 +105,7 @@ func parseHttp(s string) ParseHttpResponse {
 	}
 	return ParseHttpResponse{
 		Headers: headers,
-		Path:    path,
+		Path:    urlPath,
 		Method:  method,
 	}
 }
@@ -95,6 +118,18 @@ func responseWithBody(status int, body string) string {
 	res := fmt.Sprintf("HTTP/1.1 %d %s", status, http.StatusText(status))
 	res += crlf
 	res += "Content-Type: text/plain"
+	res += crlf
+	res += "Content-Length: " + fmt.Sprint(len(body))
+	res += crlf
+	res += crlf
+	res += body
+	return res
+}
+
+func responseWithFile(status int, body string) string {
+	res := fmt.Sprintf("HTTP/1.1 %d %s", status, http.StatusText(status))
+	res += crlf
+	res += "Content-Type: application/octet-stream"
 	res += crlf
 	res += "Content-Length: " + fmt.Sprint(len(body))
 	res += crlf
